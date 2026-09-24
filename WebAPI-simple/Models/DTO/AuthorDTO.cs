a3e33d46ee5849ffed1b77724eb5c392ad15687a
NgocLanVy <lengoclanvy1612@gmail.com>
2026-09-24 16:20:08 +0700

    Phan 5: Author, Publisher CRUD + bai tap lay ds 1 nxb va lay ds tac gia nay viet

---
+++ WebAPI-simple/Models/DTO/AuthorDTO.cs
@@ -0,0 +1,18 @@
+﻿namespace WebAPI_simple.Models.DTO
+{
+    public class AuthorDTO
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+    }
+
+    public class AuthorNoIdDTO
+    {
+        public string FullName { get; set; }
+    }
+    public class AuthorWithBooksDTO
+    {
+        public string FullName { get; set; }
+        public List<string> BookTitles { get; set; }
+    }
+}
---
+++ WebAPI-simple/Repositories/SQLAuthorRepository.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI_simple.Data;
+using WebAPI_simple.Models.Domain;
+using WebAPI_simple.Models.DTO;
+
+namespace WebAPI_simple.Repositories
+{
+    public class SQLAuthorRepository : IAuthorRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public SQLAuthorRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public List<AuthorDTO> GetAllAuthors()
+        {
+            // Get Data From Database - Domain Model
+            var allAuthorsDomain = _dbContext.Authors.ToList();
+
+            // Map domain models to DTOs
+            var allAuthorDTO = new List<AuthorDTO>();
+            foreach (var authorDomain in allAuthorsDomain)
+            {
+                allAuthorDTO.Add(new AuthorDTO()
+                {
+                    Id = authorDomain.Id,
+                    FullName = authorDomain.FullName
+                });
+            }
+            //return DTOs
+            return allAuthorDTO;
+        }
+
+        public AuthorNoIdDTO GetAuthorById(int id)
+        {
+            // get book Domain model from Db
+            var authorWithIdDomain = _dbContext.Authors.FirstOrDefault(x => x.Id == id);
+            if (authorWithIdDomain == null)
+            {
+                return null;
+            }
+            // Map Domain Model to DTOs
+            var authorNoIdDTO = new AuthorNoIdDTO
+            {
+                FullName = authorWithIdDomain.FullName,
+            };
+            return authorNoIdDTO;
+        }
+
+        public AddAuthorRequestDTO AddAuthor(AddAuthorRequestDTO addAuthorRequestDTO)
+        {
+            var authorDomainModel = new Author
+            {
+                FullName = addAuthorRequestDTO.FullName,
+            };
+            //Use Domain Model to create Author
+            _dbContext.Authors.Add(authorDomainModel);
+            _dbContext.SaveChanges();
+            return addAuthorRequestDTO;
+        }
+
+        public AuthorNoIdDTO UpdateAuthorById(int id, AuthorNoIdDTO authorNoIdDTO)
+        {
+            var authorDomain = _dbContext.Authors.FirstOrDefault(n => n.Id == id);
+            if (authorDomain != null)
+            {
+                authorDomain.FullName = authorNoIdDTO.FullName;
+                _dbContext.SaveChanges();
+            }
+            return authorNoIdDTO;
+        }
+
+        public Author? DeleteAuthorById(int id)
+        {
+            var authorDomain = _dbContext.Authors.FirstOrDefault(n => n.Id == id);
+            if (authorDomain != null)
+            {
+                _dbContext.Authors.Remove(authorDomain);
+                _dbContext.SaveChanges();
+            }
+            return authorDomain;
+        }
+        public AuthorWithBooksDTO? GetBooksByAuthorId(int id)
+        {
+            // Lấy Author kèm Book_Authors -> Book
+            var authorDomain = _dbContext.Authors
+                .Include(a => a.Book_Authors)
+                    .ThenInclude(ba => ba.Book)
+                .FirstOrDefault(a => a.Id == id);
+            if (authorDomain == null)
+            {
+                return null;
+            }
+            var result = new AuthorWithBooksDTO
+            {
+                FullName = authorDomain.FullName,
+                BookTitles = authorDomain.Book_Authors
+                    .Select(ba => ba.Book.Title)
+                    .ToList()
+            };
+            return result;
+        }
+    }
+}
